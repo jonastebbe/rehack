8ef708b8ce5a21a76239d4b8d714c59703023c2d
jonastebbe <jonastebbe@googlemail.com>
2015-04-27 09:15:03 +0200

    landingpage styling

--- app/components/public/Landing.js
+++ app/components/public/Landing.js
@@ -14,30 +14,51 @@ import Logo from '../assets/logo.js';
 const Landing = React.createClass({
         render() {
             return (
-                <div className="hero-section">
-                    <Header />
-                    <Grid className="hero-section__content">
-                        <Row>
-                            <Col xs={12} className="text-center">
-                                <Logo scale={1}/>
-                                <h1>Prosper</h1>
-                                <p className="lead">
-                                    We make savings automated and unnoticable so you can invest more.
-                                </p>
-                            </Col>
-                            <Col xs={6} smOffset={4} sm={2} className="text-right">
-                                <ButtonLink to="register" bsStyle="primary" block>Sign up</ButtonLink>
-                            </Col>
-                            <Col xs={6} sm={2}>
-                                <ButtonLink to="login" bsStyle="default" block>Sign in</ButtonLink>
-                            </Col>
-                        </Row>
-                    </Grid>
-                    <Grid className="tutorial-section">
-                        <Row>
-                            <Col></Col>
-                        </Row>
-                    </Grid>
+                <div>
+                    <div className="hero-section">
+                        <Header />
+                        <Grid className="hero-section__content">
+                            <Row>
+                                <Col xs={12} className="text-center">
+                                    <Logo scale={1}/>
+                                    <h1>Prosper</h1>
+                                    <p className="lead">
+                                        We make savings automated and unnoticable so you can invest more.
+                                    </p>
+                                </Col>
+                                <Col xs={6} smOffset={4} sm={2} className="text-right">
+                                    <ButtonLink to="register" bsStyle="primary" block>Sign up</ButtonLink>
+                                </Col>
+                                <Col xs={6} sm={2}>
+                                    <ButtonLink to="login" bsStyle="default" block>Sign in</ButtonLink>
+                                </Col>
+                            </Row>
+                        </Grid>
+                    </div>
+                    <div className="tutorial-section">
+                        <Grid>
+                            <Row>
+                                <Col sm={4}>
+                                    <h5>Lorem Ipsum</h5>
+                                    <p>
+                                        Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed.
+                                    </p>
+                                </Col>
+                                <Col sm={4}>
+                                    <h5>Lorem Ipsum</h5>
+                                    <p>
+                                        Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed.
+                                    </p>
+                                </Col>
+                                <Col sm={4}>
+                                    <h5>Lorem Ipsum</h5>
+                                    <p>
+                                        Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed. Lorem Ipsum sit dolor amed.
+                                    </p>
+                                </Col>
+                            </Row>
+                        </Grid>
+                    </div>
                 </div>
         );
     }
